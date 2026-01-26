b911f1c6d457ad080f36335ece74961b7f31b790
thanish13 <thanishmalai13@gmail.com>
2026-01-26 20:28:53 +0530

    Vault for jks file

--- Spring Cloud Vault/src/main/java/org/t13/app/api/VaultController.java
+++ Spring Cloud Vault/src/main/java/org/t13/app/api/VaultController.java
@@ -1,9 +1,17 @@
 package org.t13.app.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.t13.app.dto.VaultSecretRequest;
 import org.t13.app.service.VaultWriteService;
+
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/vault")
@@ -26,4 +34,20 @@ public class VaultController {
 
         return ResponseEntity.ok("Secrets stored successfully in Vault");
     }
+
+    @PostMapping(value = "/jks", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadJks(
+            @RequestParam String app,
+            @RequestParam String type,
+            @RequestParam MultipartFile file) throws Exception {
+
+        if (!Objects.requireNonNull(file.getOriginalFilename()).endsWith(".jks")) {
+            return ResponseEntity.badRequest().body("Only .jks files allowed");
+        }
+
+        String path = vaultWriteService.writeFile(app, type, file);
+
+        return ResponseEntity.ok("Stored in Vault at " + path);
+    }
+
 }
--- Spring Cloud Vault/src/main/java/org/t13/app/service/VaultWriteService.java
+++ Spring Cloud Vault/src/main/java/org/t13/app/service/VaultWriteService.java
@@ -2,7 +2,11 @@ package org.t13.app.service;
 import org.springframework.stereotype.Service;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.VaultResponse;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +25,21 @@ public class VaultWriteService {
         data.put("data", secrets); // KV v2 requires "data"
 
         vaultTemplate.write("secret/data/" + path, data);
+    }
+
+    public String writeFile(String app, String type, MultipartFile file) throws IOException {
+
+        String base64 = Base64.getEncoder().encodeToString(file.getBytes());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put(type, base64);
+        data.put("uploadedAt", Instant.now().toString());
+
+        String path = "secret/data/" + app + "/jks";
+
+        vaultTemplate.write(path, Map.of("data", data));
+
+        return path;
     }
 }
 
