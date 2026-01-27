057cadbaa1918f65e3a0652594aeec9a65be114d
thanish13 <thanishmalai13@gmail.com>
2026-01-27 23:09:12 +0530

    Vault logs

--- Spring Cloud Vault/src/main/java/org/t13/app/dto/VaultSecretRequest.java
+++ Spring Cloud Vault/src/main/java/org/t13/app/dto/VaultSecretRequest.java
@@ -1,25 +1,15 @@
 package org.t13.app.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Map;
 
+@Setter
+@Getter
 public class VaultSecretRequest {
 
     private String path;                // ex: myapp
     private Map<String, Object> secrets; // key-value pairs
 
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public Map<String, Object> getSecrets() {
-        return secrets;
-    }
-
-    public void setSecrets(Map<String, Object> secrets) {
-        this.secrets = secrets;
-    }
 }
--- Spring Cloud Vault/src/main/java/org/t13/app/service/VaultWriteService.java
+++ Spring Cloud Vault/src/main/java/org/t13/app/service/VaultWriteService.java
@@ -1,7 +1,9 @@
 package org.t13.app.service;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.vault.core.VaultTemplate;
-import org.springframework.vault.support.VaultResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+@Log4j2
 @Service
 public class VaultWriteService {
 
@@ -17,14 +20,18 @@ public class VaultWriteService {
 
     public VaultWriteService(VaultTemplate vaultTemplate) {
         this.vaultTemplate = vaultTemplate;
+        log.info("Initialize VaultWriteService Bean");
     }
 
     public void writeSecret(String path, Map<String, Object> secrets) {
 
         Map<String, Object> data = new HashMap<>();
         data.put("data", secrets); // KV v2 requires "data"
+        log.info("Write secret to path : {}",path);
 
         vaultTemplate.write("secret/data/" + path, data);
+
+        log.info("Write secret to path : {} successfully" ,path);
     }
 
     public String writeFile(String app, String type, MultipartFile file) throws IOException {
@@ -37,7 +44,11 @@ public class VaultWriteService {
 
         String path = "secret/data/" + app + "/jks";
 
+        log.info("Write file to path : {}",path);
+
         vaultTemplate.write(path, Map.of("data", data));
+
+        log.info("Write file success to path : {} successfully",path);
 
         return path;
     }
