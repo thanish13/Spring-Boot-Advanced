8c0812268df767ccc49b3a03fe8a6d8d41130ff1
thanish13 <thanishmalai13@gmail.com>
2026-01-31 23:03:11 +0530

    EH Cache setup

--- EH-Cache/src/main/java/org/t13/app/config/CachingConfig.java
+++ EH-Cache/src/main/java/org/t13/app/config/CachingConfig.java
@@ -13,7 +13,6 @@ import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.jcache.JCacheCacheManager;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
