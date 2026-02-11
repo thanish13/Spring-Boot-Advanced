5bea3706ab3423b9e75075bab5ce24554e60196c
thanish13 <thanishmalai13@gmail.com>
2026-02-11 21:44:53 +0530

    SOAP setup

--- SOAP-SSL/src/main/java/org/t13/app/config/WebServiceConfig.java
+++ SOAP-SSL/src/main/java/org/t13/app/config/WebServiceConfig.java
@@ -4,15 +4,17 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
+@EnableWs
 public class WebServiceConfig {
 
-    @Bean(name = "students")
+    @Bean
     public DefaultWsdl11Definition defaultWsdl11Definition(SimpleXsdSchema studentSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("StudentPort");
@@ -23,16 +25,17 @@ public class WebServiceConfig {
     }
 
     @Bean
-    public SimpleXsdSchema studentSchema() {
-        return new SimpleXsdSchema(new ClassPathResource("students.xsd"));
-    }
-
-    @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean<>(servlet, "/ws/*");
     }
+
+    @Bean
+    public SimpleXsdSchema studentSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("students.xsd"));
+    }
+
 
 }
